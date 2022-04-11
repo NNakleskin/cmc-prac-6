b84c46912c4ae9c53b325b5d81051e9e0074a5c7
Nikita Nakleskin <nakleskin.nv@gmail.com>
2022-04-11 23:12:50 +0300

    Added root function

--- main.c
+++ main.c
@@ -2,10 +2,12 @@
 #include "math.h"
 #include "stdlib.h"
 
+#define NOT_SUPPORTED_VAR -1
+
 
 double f(double x, int var)
 {
-    if(var)
+    if(var == 1)
     {
         return 1 / exp(x) + 3;
     }
@@ -17,10 +19,26 @@ double f(double x, int var)
     {
         return 1 / x;
     }
-    return 0;
+    return NOT_SUPPORTED_VAR;
 }
 
-double root(double(*f), double(*g), int a, int b, double eps1);
+double root(double (*f)(double, int), double(*g)(double, int), double a, double b, double eps1, int var)
+{
+    double x = 0;
+    while(fabs(a + b) > 2 * eps1)
+    {
+        x = (a + b) / 2;
+        if(f(x, var) * f(a, var) < 0)
+        {
+            b = x;
+        }
+        if(f(x, var) * f(a, var) < 0)
+        {
+            a = x;
+        }
+    }
+    return x;
+}
 
 
 double integral(double(*f), int a, int b, double eps2);
@@ -29,7 +47,9 @@ double integral(double(*f), int a, int b, double eps2);
 int main(void)
 {
     double x;
+    int a, b;
     
+    printf("%lf", f(12.0, 4));
     return 0;
 }
 
