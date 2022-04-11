c71c39c67fe5e956290ca315c721d5e75bf05f5e
Nikita Nakleskin <nakleskin.nv@gmail.com>
2022-04-11 20:08:12 +0300

    initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+#include "math.h"
+#include "stdlib.h"
+
+
+double root(f, g, a, b, double eps1);
+
+
+double integral(f, a, b, eps2);
+
+
+int main(void)
+{
+    double x;
+    
+    return 0;
+}
+
