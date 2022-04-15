db6fbec5b131b4d5a5c6c45c7da783c65a154db2
Nikita Nakleskin <nakleskin.nv@gmail.com>
2022-04-16 00:51:13 +0300

    Variable names changed

--- main.c
+++ main.c
@@ -1,42 +1,42 @@
-#include "stdio.h"
-#include "math.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
 
 
-double max(double a, double b)
+double max(double first_number, double second_number)
 {
-    if(a > b)
+    if(first_number > second_number)
     {
-        return a;
+        return first_number;
     }
-    return b;
+    return second_number;
 }
 
-double min(double a, double b)
+double min(double first_number, double second_number)
 {
-    if(a < b)
+    if(first_number < second_number)
     {
-        return a;
+        return first_number;
     }
-    return b;
+    return second_number;
 }
 
 
-double max3(double a, double b, double c)
+double max3(double first_number, double second_number, double third_number)
 {
-    return max(max(a, b), c);
+    return max(max(first_number, second_number), third_number);
 }
 
 
-double min3(double a, double b, double c)
+double min3(double first_number, double second_number, double third_number)
 {
-    return min(min(a, b), c);
+    return min(min(first_number, second_number), third_number);
 }
 
 
-double mid3(double a, double b, double c)
+double mid3(double first_number, double second_number, double third_number)
 {
-    return min3(max(a, b), max(b, c), max(a, c));
+    return min3(max(first_number, second_number), max(second_number, third_number), max(first_number, third_number));
 }
 
 
@@ -58,7 +58,8 @@ double f3(double x)
 }
 
 
-double square_int(double (*f)(double), double x, double delt)
+double rieman_integral(double (*f)(double), double x, double delt)
+//Calculation of one term of the Riemann integral sum
 {
     return f((x + delt / 2)) * delt;
 }
@@ -91,7 +92,7 @@ double integral(double(*f)(double), double a, double b, double eps)
     double result = 0;
     for(double i = 0; i < n; i++)
     {
-        result += square_int(f, x, delt);
+        result += rieman_integral(f, x, delt);
         x += delt;
     }
     return result;
@@ -120,6 +121,45 @@ int main(void)
     printf("%lf\n", mid3(3, 2, 1));
     printf("%lf\n", mid3(2, 3, 1));
     printf("%lf\n", mid3(1, 2, 3));
+    
+    double a, b, c;
+    a = root(f1, f2, 0, 100, 0.1);
+    b = root(f3, f2, 0, 100, 0.1);
+    c = root(f1, f3, 0, 100, 0.1);
+    
+    if(min3(a, b, c) == a)
+    {
+        if(max3(a, b, c) == b)
+        {
+            // f2
+        }
+        else // max3(a, b, c) == c
+        {
+            //f1
+        }
+    }
+    else if(min3(a, b, c) == b)
+    {
+        if(max3(a, b, c) == a)
+        {
+            // f2
+        }
+        else // max3(a, b, c) == c
+        {
+            //f3
+        }
+    }
+    else // min3(a, b, c) == c
+    {
+        if(max3(a, b, c) == a)
+        {
+            // f1
+        }
+        else // max3(a, b, c) == b
+        {
+            //f3
+        }
+    }
     
     return 0;
 }
