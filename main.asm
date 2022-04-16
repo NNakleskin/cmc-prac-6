c55e34568d8f0ce94c25dd3f604814265979ae3c
Nikita <n.nakleskin@yandex.ru>
2022-04-16 14:19:31 +0300

    Creating f1 func

--- main.asm
+++ main.asm
@@ -1,26 +1,68 @@
-%include "io.inc"
+
+
+section .data
+    op db `%lf`, 0
+
+section .bss
+    x resb 8
 
 section .text
-CEXTERN exp
 
 f1:
     push ebp
     mov ebp, esp
     push ecx
     push ebx
-    
-    mov ebx, dword[esp + 8]
-    mov eax, 1
-    cdq
-    div eax
-    ;непонятно как работать с double при делении
-    
+    finit
+    fld1
     add eax, 3
-    
-    
 
+f2:
+
+
+f3:
+    push ebp
+    mov ebp, esp
+    push ecx
+    push ebx
+    finit
+    fld1
+    fld qword[esp + 8]
+    fdivp
+    fstp qword[eax]
+    add eax, 3
+    pop ebx
+    pop ecx
+    mov esp, ebp
+    pop ebp
+    ret
+
+
+    
+    
+extern scanf
+extern printf
 global CMAIN
 CMAIN:
-    
+    sub esp, 4
+    push x
+    push op
+
+    call scanf
+
+    add esp, 12
+
+
+    push x
+    call f3
+    add esp, 8
+
+
+    sub esp, 4
+    push dword[x]
+    push op
+    call printf
+    add esp, 12
+
     xor eax, eax
     ret
