72d6cf1cb0296dd55e1903ede0516c98a3c230ed
Nikita Nakleskin <nakleskin.nv@gmail.com>
2022-04-16 18:26:15 +0300

    Added f3 func

---
+++ functions.asm
@@ -0,0 +1,46 @@
+%include "io.inc"
+
+section .rodata
+    op db `%lf`, 0
+
+CEXTERN scanf
+CEXTERN printf
+
+section .text
+
+f3:
+    mov ecx, esp
+    and esp, 0xfffffff0
+    push ecx
+    sub esp, 28
+    lea eax, [esp+20]
+
+    mov [esp + 4], eax
+    mov dword[esp], op
+    call scanf
+    cmp eax, 1
+    jne .epilog
+
+    finit 
+    fld1
+    fld qword[esp + 20]
+    fdivp
+    fstp qword[esp + 20]
+
+    mov eax, [esp + 20]
+    mov [esp + 4], eax
+    mov eax, [esp + 24]
+    mov [esp + 8], eax
+    mov dword[esp], op
+    call printf
+
+    .epilog:
+    add esp, 28
+    pop ecx
+    mov esp, ecx
+
+global CMAIN
+CMAIN:
+    call f3
+    xor eax, eax
+    ret
