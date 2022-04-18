a4e0d383c317888b5b44337d3417f7b42f3a1d71
Nikita <n.nakleskin@yandex.ru>
2022-04-18 03:44:40 +0300

    Fixed f1 function

--- functions.asm
+++ functions.asm
@@ -1,23 +1,25 @@
 section .rodata
     a dw 2
-    b dw 4
+    b dw 3
+    
 
 section .text
 global f1
 global f2
 global f3
+extern exp
 
 
 f1:
     fld qword[esp + 4]
+    fchs
     sub esp, 8
     fstp qword[esp]
     call exp
-    add esp, 8  
+    add esp, 8
     fild word[b]
     faddp
     ret
-
 
 f2:
     fld qword[esp + 4]
@@ -26,7 +28,7 @@ f2:
     fild word[a]
     fsubp
     ret
-
+            
 f3:
     fld1
     fld qword[esp + 4] 
