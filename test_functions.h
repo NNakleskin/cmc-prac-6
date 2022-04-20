1104e0e78d078298ddf94dd3ef9ddadf8c9b7a58
Nikita <n.nakleskin@yandex.ru>
2022-04-20 23:28:30 +0300

    Added f4-f8 functions

---
+++ test_functions.h
@@ -0,0 +1,24 @@
+double f4(double x)
+{
+    return 2 * x * x;
+}
+
+double f5(double x)
+{
+    return 8 + 2 * x - x * x;
+}
+
+double f6(double x)
+{
+    return 2 * x + 1;
+}
+
+double f7(double x)
+{
+    return 6 * x;
+}
+
+double f8(double x)
+{
+    return x;
+}
