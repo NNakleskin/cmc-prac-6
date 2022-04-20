ebda079853b172c84180917627dc7f3c67b07ea8
Nikita <n.nakleskin@yandex.ru>
2022-04-20 21:44:40 +0300

    Added multikey support

--- main.c
+++ main.c
@@ -27,73 +27,76 @@ double f3(double x)
 int main(int argc, char **argv)
 {
     short status = 0;
-    if(!strcmp(argv[1], "-help"))
+    for(int i = 0; i < argc; i++)
     {
-        status = 1;
-        help();
-    }
-    if(!strcmp(argv[1], "-test-f1"))
-    {
-        status = 1;
-        if(argc == 3)
+        if(!strcmp(argv[i], "-help"))
         {
-            test_f1(strtod(argv[2], NULL));		
+            status = 1;
+            help();
         }
-        else
+        if(!strcmp(argv[i], "-test-f1"))
         {
-            puts("Argument not found");
+            status = 1;
+            if(argc == 3)
+            {
+                test_f1(strtod(argv[2], NULL));		
+            }
+            else
+            {
+                puts("Argument not found");
+            }
         }
-    }
-    if(!strcmp(argv[1], "-test-f2"))
-    {
-        status = 1;
-        if(argc == 3)
+        if(!strcmp(argv[i], "-test-f2"))
         {
-            test_f2(strtod(argv[2], NULL));		
+            status = 1;
+            if(argc == 3)
+            {
+                test_f2(strtod(argv[2], NULL));		
+            }
+            else
+            {
+                puts("Argument not found");
+            }
         }
-        else
+        if(!strcmp(argv[i], "-test-f3"))
         {
-            puts("Argument not found");
+            status = 1;
+            if(argc == 3)
+            {
+                test_f3(strtod(argv[2], NULL));		
+            }
+            else
+            {
+                puts("Argument not found");
+            }
         }
-    }
-    if(!strcmp(argv[1], "-test-f3"))
-    {
-        status = 1;
-        if(argc == 3)
+        if(!strcmp(argv[i], "-test-integral"))
         {
-            test_f3(strtod(argv[2], NULL));		
+            status = 1;
+            test_integral();
         }
-        else
+        if(!strcmp(argv[i], "-test-root"))
         {
-            puts("Argument not found");
+            status = 1;
+            test_root();
         }
-    }
-    if(!strcmp(argv[1], "-test-integral"))
-    {
-        status = 1;
-        test_integral();
-    }
-    if(!strcmp(argv[1], "-test-root"))
-    {
-        status = 1;
-        test_root();
-    }
-    if(!strcmp(argv[1], "-solve"))
-    {
-        status = 1;
-        solve();
-    }
-    if(!strcmp(argv[2], "-show-absciss"))
-    {
-        show_absciss();
-    }
-    if(!strcmp(argv[2], "-show-count"))
-    {
-        show_count();
-    }
-    if(!status)
-    {
-        help();
+        if(!strcmp(argv[i], "-solve"))
+        {
+            status = 1;
+            solve();
+        }
+        if(!strcmp(argv[i], "-show-absciss"))
+        {
+            show_absciss();
+        }
+        if(!strcmp(argv[i], "-show-count"))
+        {
+            show_count();
+        }
+        if(!status)
+        {
+            help();
+        }
     }
     return 0;
 }
